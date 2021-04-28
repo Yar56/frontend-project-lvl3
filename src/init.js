e802331499e1067891afb742d8d00a04d4a6f73a
Yaroslav Vavilin <yarikvavilin@gmail.com>
2021-04-28 17:02:34 +0500

    add query to rss(need fix)

--- src/init.js
+++ src/init.js
@@ -1,45 +1,91 @@
 import onChange from 'on-change';
+import axios from 'axios';
 import validateUrl from './validateUrl.js';
 // import * as yup from 'yup';
 
 export default () => {
   const state = {
     formState: {
-      state: 'pending', // sending
-      validUrl: true,
-      validRss: true,
-      messages: {
-        errors: {},
-        success: [],
-      },
+      processState: 'pending', // sending
+      processError: null,
+      processSucces: null,
+      valid: true,
+      validError: '',
 
     },
     feed: [],
     post: [],
   };
-  const renderFeedback = (watchedState) => {
+  const form = document.querySelector('.rss-form');
+  const submitButton = document.querySelector('button[type="submit"]');
+
+  const renderFeedback = (value) => {
     const divFeedBack = document.querySelector('.feedback');
-    if (!watchedState.formState.validUrl) {
+    console.log(value);
+    divFeedBack.classList.remove('text-success', 'text-danger');
+    if (value === 'RSS успешно загружен') {
+      divFeedBack.classList.add('text-success');
+    } else {
       divFeedBack.classList.add('text-danger');
-      divFeedBack.textContent = watchedState.formState.messages.errors.url;
     }
+    divFeedBack.textContent = value;
   };
 
-  const watchedState = onChange(state, (path) => {
-    if (path === 'formState.validUrl') {
-      renderFeedback(watchedState);
+  const processStateHandle = (processState, watchedState) => {
+    if (processState === 'pending') {
+      submitButton.disabled = false;
+    } else if (processState === 'sending') {
+      submitButton.disabled = true;
+    } else if (processState === 'finished') {
+      submitButton.disabled = false;
+      renderFeedback(watchedState.formState.processSucces);
+      form.reset();
+      // рендер фидов и постов
+    } else if (processState === 'failed') {
+      renderFeedback(watchedState.formState.processError);
+    }
+  };
+  const watchedState = onChange(state, (path, value) => {
+    if (path === 'formState.processState') {
+      processStateHandle(value, watchedState);
+    }
+    if (path === 'formState.valid') {
+      // const divFeedBack = document.querySelector('.feedback');
+      if (!value) {
+        renderFeedback(watchedState.formState.validError);
+      }
     }
   });
 
-  const form = document.querySelector('.rss-form');
   form.addEventListener('submit', (e) => {
     e.preventDefault();
+    watchedState.formState.processState = 'sending';
+
     const data = new FormData(form);
-    const url = data.get('url');
-    const isValid = validateUrl(url);
+    const queryString = data.get('url');
+    const isValid = validateUrl(queryString);
+
     if (!isValid) {
-      watchedState.formState.messages.errors.url = 'Ссылка должна быть валидным URL';
-      watchedState.formState.validUrl = isValid;
+      watchedState.formState.validError = 'Ссылка должна быть валидным URL';
+      watchedState.formState.processState = 'pending';
+      watchedState.formState.valid = isValid;
+    }
+    if (isValid) {
+      // watchedState.formState.valid = isValid;
+      axios.get(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(queryString)}`)
+        .then((response) => {
+          watchedState.formState.processSucces = 'RSS успешно загружен';
+          watchedState.formState.processState = 'finished';
+          // watchedState.formState.processState = 'pending';
+          console.log(response);
+        })
+        .catch((error) => {
+          watchedState.formState.processState = 'pending';
+          watchedState.formState.processError = 'Ошибка сети';
+          watchedState.formState.processState = 'failed';
+          console.log(error);
+        });
     }
   });
+  console.log(state);
 };
