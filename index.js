0cd72374191d61de3c294fdc9cf0b9bc0eabb8ff
Yaroslav Vavilin <yarikvavilin@gmail.com>
2021-05-13 12:10:03 +0500

    fix3 index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import 'bootstrap/js/dist/modal';
+import 'bootstrap';
+// import 'bootstrap/js/dist/modal';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import runApp from './src/init.js';
 
-// runApp();
-export default runApp();
+runApp();
+// export default runApp();
